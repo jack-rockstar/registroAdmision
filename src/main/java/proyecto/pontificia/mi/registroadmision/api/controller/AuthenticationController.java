02e0b4addd982d58dd505819890796c192603b52
Jack Najarro <jacknajarro1@gmail.com>
2023-07-08 12:51:50 -0500

    nts

--- src/main/java/proyecto/pontificia/mi/registroadmision/api/controller/AuthenticationController.java
+++ src/main/java/proyecto/pontificia/mi/registroadmision/api/controller/AuthenticationController.java
@@ -1,6 +1,8 @@
 package proyecto.pontificia.mi.registroadmision.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import proyecto.pontificia.mi.registroadmision.api.DTO.LoginDto;
 import proyecto.pontificia.mi.registroadmision.api.model.Usuario;
@@ -15,19 +17,20 @@ public class AuthenticationController {
     private AuthenticationService authenticationService;
 
     @PostMapping("/login")
-    public Response login(@RequestBody LoginDto usuario) {
+    public ResponseEntity<Response> login(@RequestBody LoginDto usuario) {
         Response respuesta = new Response();
         try {
             Usuario user = authenticationService.login(usuario);
             respuesta.setCodigoRespuesta(200);
             respuesta.setMensajeRespuesta("Operacion correcta");
             respuesta.setData(user);
+            return  ResponseEntity.ok(respuesta);
         } catch (Exception e) {
             respuesta.setCodigoRespuesta(500);
             respuesta.setMensajeRespuesta("Error en la operacion: " + e.getMessage());
             respuesta.setData(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(respuesta);
         }
-        return respuesta;
     }
 
 }
